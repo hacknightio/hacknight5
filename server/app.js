da50b0bd0d3f02e85d2f0955683949281e2c1444
Jacob Badger <jake.badger17@gmail.com>
2019-09-05 13:50:35 -0600

    fixed

--- server/app.js
+++ server/app.js
@@ -1,14 +1,14 @@
 const express = require('express')
 const app = express()
- 
+
+app.use(express.json())
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
 app.post('/misty/api/drive', function (req, res) {
-  const {linearVelocity, angularVelocity} = req.body
-  if (linearVelocity && angularVelocity){
-    console.log(linearVelocity, angularVelocity)
+  const { LinearVelocity, AngularVelocity } = req.body
+  if (LinearVelocity && AngularVelocity){
     res.send(true)
   }
   else {

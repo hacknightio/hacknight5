8f9a0e9a765227e6a9e8da0bed65e8e7d1ac07b1
david3214 <davidw3214@gmail.com>
2019-09-05 20:43:02 -0600

    Added all non alpha movement api endpoints

--- server/app.js
+++ server/app.js
@@ -7,6 +7,7 @@ app.get('/', function(req, res) {
   res.send('Hello World')
 })
 
+// Drive
 app.post('/misty/api/drive', function(req, res) {
   const { LinearVelocity, AngularVelocity } = req.body
   if (LinearVelocity && AngularVelocity) {
@@ -20,6 +21,7 @@ app.post('/misty/api/drive', function(req, res) {
   }
 })
 
+// DriveTime
 app.post('/misty/api/drive/time', function(req, res) {
   // Degree is optional
   const { LinearVelocity, AngularVelocity, TimeMs, Degree } = req.body
@@ -34,6 +36,7 @@ app.post('/misty/api/drive/time', function(req, res) {
   }
 })
 
+// DriveTrack
 app.post('/misty/api/drive/track', function(req, res) {
   const { LeftTrackSpeed, RightTrackSpeed } = req.body
   if (LeftTrackSpeed && RightTrackSpeed) {
@@ -45,6 +48,59 @@ app.post('/misty/api/drive/track', function(req, res) {
   } else {
     res.send(false)
   }
+})
+
+// MoveArm
+app.post('/misty/api/arms', function(req, res) {
+  // Velocity and Units are optional
+  const { Arm, Position, Velocity, Units } = req.body
+  if (Arm && Position) {
+    if (validTypes([Position], 'number') && validTypes([Arm], 'string')) {
+      res.send(true)
+    } else {
+      res.send(false)
+    }
+  } else {
+    res.send(false)
+  }
+})
+
+// MoveArms
+app.post('/misty/api/arms/set', function(req, res) {
+  // all inputs are optional
+  const {
+    LeftArmPosition,
+    RightArmPosition,
+    LeftArmVelocity,
+    RightArmVelocity,
+    Units
+  } = req.body
+  res.send(true)
+})
+
+// MoveHead
+app.post('/misty/api/head', function(req, res) {
+  // Velocity and Units are optional
+  const { Pitch, Roll, Yaw, Velocity, Units } = req.body
+  if ((Roll && Pitch, Yaw)) {
+    if (validTypes([Roll, Pitch, Yaw], 'number')) {
+      res.send(true)
+    } else {
+      res.send(false)
+    }
+  } else {
+    res.send(false)
+  }
+})
+
+// Halt
+app.post('/misty/api/halt', function(req, res) {
+  // No parameters and return value
+})
+
+// Stop
+app.post('/misty/api/drive/stop', function(req, res) {
+  // No parameters and return value
 })
 
 app.listen(3000)

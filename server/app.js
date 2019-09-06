bcc647f337ff137c5f95edd2cfe605522701cbf2
david3214 <davidw3214@gmail.com>
2019-09-05 19:14:29 -0600

    Beginning to start on mocking out the endpoints, just clearing state with Jake

--- server/app.js
+++ server/app.js
@@ -1,29 +1,57 @@
 const express = require('express')
 const app = express()
-const WebSocket = require('ws')
-
-const ws = new WebSocket('http://localhost:4000/')
 
 app.use(express.json())
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
   res.send('Hello World')
 })
 
-app.post('/misty/api/drive', function (req, res) {
+app.post('/misty/api/drive', function(req, res) {
   const { LinearVelocity, AngularVelocity } = req.body
-  if (LinearVelocity && AngularVelocity){
-    if (typeof(LinearVelocity) === 'number' && typeof(AngularVelocity) === 'number'){
+  if (LinearVelocity && AngularVelocity) {
+    if (validTypes([LinearVelocity, AngularVelocity], 'number')) {
       res.send(true)
-      ws.send('something')
-    }
-    else {
+    } else {
       res.send(false)
     }
+  } else {
+    res.send(false)
   }
-  else{
+})
+
+app.post('/misty/api/drive/time', function(req, res) {
+  // Degree is optional
+  const { LinearVelocity, AngularVelocity, TimeMs, Degree } = req.body
+  if (LinearVelocity && AngularVelocity && TimeMs) {
+    if (validTypes([LinearVelocity, AngularVelocity, TimeMs], 'number')) {
+      res.send(true)
+    } else {
+      res.send(false)
+    }
+  } else {
+    res.send(false)
+  }
+})
+
+app.post('/misty/api/drive/track', function(req, res) {
+  const { LeftTrackSpeed, RightTrackSpeed } = req.body
+  if (LeftTrackSpeed && RightTrackSpeed) {
+    if (validTypes([LinearVelocity, AngularVelocity, TimeMs], 'number')) {
+      res.send(true)
+    } else {
+      res.send(false)
+    }
+  } else {
     res.send(false)
   }
 })
 
 app.listen(3000)
+
+function validTypes(Args, type) {
+  return Args.reduce(
+    (status, nextArg) => (status ? typeof nextArg === type : status),
+    true
+  )
+}

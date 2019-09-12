e9afd6f959d021f67b7ebaac23bac8434f2e9602
Joshua Young <joshua.r.young1@gmail.com>
2019-09-12 14:07:48 -0600

    pre merge

---
+++ bin/echo.js
@@ -0,0 +1,6 @@
+const data = require('../docs/temp')
+Object.keys(data.result).forEach(method => {
+  data.result[method].forEach(command => {
+    console.log(`${method} /${command.endpoint}`)
+  })  
+})
